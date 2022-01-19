a0acf6d3a93f343fc351993957ccec91e6df98ba
Fuad Alamoudi <fuad.h.alamoudi@gmail.com>
2022-01-19 12:58:15 +0300

    Finsh From Timer

--- js/main.js
+++ js/main.js
@@ -15,3 +15,21 @@ window.onclick = function(e) {
         modallogin.style.display = "none";
     }
 }
+// Start Timair
+const advContainer = document.querySelector(".adv-container");
+const closeBtn = document.querySelector(".adv-container .close");
+const advLink=document.querySelector(".adv-container a");
+
+if (!localStorage.getItem("DisplayingAdv")){
+setTimeout(() => {
+    advContainer.classList.add("activ");
+}, 1000);
+}
+
+const stopDisplayingAdv = ()=> {
+    localStorage.setItem("DisplayingAdv",false);
+    advContainer.classList.remove("activ")
+};
+closeBtn.addEventListener("click",stopDisplayingAdv);
+advLink.addEventListener("click",stopDisplayingAdv);
+// End Timair
